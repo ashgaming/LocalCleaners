aaae70f70fcb1fe2a6492029d96d7439efc115d9
ashgaming <68996995+ashgaming@users.noreply.github.com>
2025-01-14 14:10:13 +0530

    app name

--- frontend/src/pages/admin/Dashboard.jsx
+++ frontend/src/pages/admin/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Sidebar from '../../components/Admin/Sidebar';
-const FeedBacks = React.lazy(() => import( '../../components/Admin/FeedBacks'));
+import { Menu } from 'lucide-react';
+const FeedBacks = React.lazy(() => import('../../components/Admin/FeedBacks'));
 const Analytics = React.lazy(() => import('../../components/Admin/Analytics'));
 const Services = React.lazy(() => import('../../components/Admin/Services'));
 const Users = React.lazy(() => import('../../components/Admin/Users'));
@@ -10,6 +11,8 @@ const Wrapper = React.lazy(() => import('../../components/ui/Wrapper'));
 
 const AdminDashboard = () => {
     const [activeSection, setActiveSection] = useState('analytics');
+
+    const [isSidebarOpen, setIsSidebarOpen] = useState(true)
 
     const renderContent = () => {
         switch (activeSection) {
@@ -30,13 +33,20 @@ const AdminDashboard = () => {
         }
     };
 
+
+
     return (
-        <div className="flex min-h-screen bg-gray-100">
-            <Sidebar activeSection={activeSection} onSectionChange={setActiveSection} />
-            <main className="flex-1 overflow-auto">
-            <Wrapper>
-                {renderContent()}
-               </Wrapper>
+        <div className="flex h-screen min-h-screen bg-gray-100 overflow-hidden">
+            { isSidebarOpen && <Sidebar activeSection={activeSection} onSectionChange={setActiveSection} setIsSidebarOpen={setIsSidebarOpen} />  }
+            <main className="flex-1 overflow-y-scroll">
+                {!isSidebarOpen && <div className="bg-gray-900 text-white w-full h-15 p-4 ">
+                    <button onClick={() => setIsSidebarOpen(true)} className="text-xl font-bold">
+                        <Menu />
+                    </button>
+                </div>}
+                <div>
+                    <Wrapper>{renderContent()}</Wrapper>
+                </div>
             </main>
         </div>
     );
